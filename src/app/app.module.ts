9dbf2ede26da2f02989966e3a4fb2e379686c886
andrictijana <andrictijana96@gmail.com>
2020-04-22 17:12:47 +0200

    Frontend prvi - bez formi za update i add domain

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DomainListComponent } from './components/domain-list/domain-list.component';
+import { DomainService } from './services/domain.service';
+import {Routes, RouterModule} from '@angular/router';
+import { UpdateDomainComponent } from './components/update-domain/update-domain.component';
+import { AddDomainComponent } from './components/add-domain/add-domain.component';
+
+
+const routes: Routes = [
+  {path : 'domain/:id' , component: DomainListComponent},
+  {path : 'domains', component: DomainListComponent},
+  {path : 'add' , component: AddDomainComponent},
+  {path : 'update/:id' , component: UpdateDomainComponent},
+  {path : '' , redirectTo:'/domains', pathMatch: 'full'},
+  {path : '**' , redirectTo:'/domains', pathMatch: 'full'}
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DomainListComponent,
+    UpdateDomainComponent,
+    AddDomainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [DomainService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
